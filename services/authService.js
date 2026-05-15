e1ba6118d2105f36dc46c21c9a6fc8c92fad90c1
partheban-M <partheban61@gmail.com>
2026-05-15 15:06:43 +0530

    renamed

---
+++ services/authService.js
@@ -0,0 +1,31 @@
+
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+
+const registerUser = async (userData) => {
+  const { name, email, password } = userData;
+
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    throw new Error('User already exists');
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  return {
+    id: user._id,
+    name: user.name,
+    email: user.email,
+  };
+};
+
+module.exports = {
+  registerUser,
+};
