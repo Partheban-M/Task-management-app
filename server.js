070c2d0943e7a5df88c4976cbeddebcd30c13734
partheban-M <partheban61@gmail.com>
2026-05-15 15:56:10 +0530

    upation server

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const authRoutes = require('./routes/authRoutes');
-const connectDB = require('./config/db');
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
 
 dotenv.config();
 connectDB();
@@ -11,6 +12,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
