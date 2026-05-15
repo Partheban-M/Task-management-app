8b9a741c84ccfac17d4889c4b2ae93e28cd17629
partheban-M <partheban61@gmail.com>
2026-05-15 18:27:43 +0530

    add taskroutes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
+const taskRoutes = require('./routes/taskRoutes');
 
 dotenv.config();
 connectDB();
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
 
 app.get('/', (req, res) => {
   res.send('Task Manager API Running');
