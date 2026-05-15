b45cd46bc30225eb847bfc2ae7de4cde2649b002
partheban-M <partheban61@gmail.com>
2026-05-15 15:27:18 +0530

    conecting to service

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const authRoutes = require('./routes/authRoutes');
 const connectDB = require('./config/db');
 const express = require('express');
 const dotenv = require('dotenv');
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('Task Manager API Running');
